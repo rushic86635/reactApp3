b8d0a5adc08c5f819fc7b6c2deb8886182b310a9
Rushikesh Chougule <rushi.sunbeam2024@gmail.com>
2024-11-28 19:55:36 +0530

    First Commit app3

--- src/App.js
+++ src/App.js
@@ -3,21 +3,9 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="header">
+      <h2>Hello Rushikesh</h2>
+      <div> I am pursuing CDAC at Sunbeam Karad !!!</div>
     </div>
   );
 }
